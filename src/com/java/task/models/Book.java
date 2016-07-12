df905b867eb2840fd543b6ace8a2f70fec61b839
markusFrost <andreywystawkin@yandex.ru>
2016-07-12 21:06:08 +0300

    create fake generation algorithm

--- src/com/java/task/models/Book.java
+++ src/com/java/task/models/Book.java
@@ -1,3 +1,4 @@
+package com.java.task.models;
 
 public class Book {
 
---
+++ src/com/java/task/utils/FakeHelper.java
@@ -0,0 +1,84 @@
+package com.java.task.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.java.task.models.Book;
+import com.java.task.models.Reader;
+
+public class FakeHelper {
+
+	public static List<Book> generateFakeData() {
+
+		Reader reader = new Reader();
+		reader.setName("John Smith");
+		reader.setReadingSpeed(90);
+
+		List<Book> listBooks = new ArrayList<Book>();
+		Book b;
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("Jack London");
+		b.setTitle("White Fang");
+		b.setNumberOfPages(215);
+		b.setOverdueTime(2);
+
+		listBooks.add(b);
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("Jonathan Swift");
+		b.setTitle("Gulliver’s Travels");
+		b.setNumberOfPages(352);
+		b.setOverdueTime(3);
+
+		listBooks.add(b);
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("Magda Szabo");
+		b.setTitle("The Door");
+		b.setNumberOfPages(400);
+		b.setOverdueTime(4);
+
+		listBooks.add(b);
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("JRR Tolkien");
+		b.setTitle("The Lord of the Rings");
+		b.setNumberOfPages(700);
+		b.setOverdueTime(6);
+
+		listBooks.add(b);
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("Steven Erikson");
+		b.setTitle("A Tale of the Malazan Book of the Fallen");
+		b.setNumberOfPages(300);
+		b.setOverdueTime(3);
+
+		listBooks.add(b);
+
+		// --------------------------------
+
+		b = new Book();
+		b.setAuthor("Brandon Sanderson");
+		b.setTitle("The Stormlight Archive");
+		b.setNumberOfPages(500);
+		b.setOverdueTime(5);
+
+		listBooks.add(b);
+		
+		return listBooks;
+
+	}
+
+}
