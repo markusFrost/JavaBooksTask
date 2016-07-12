3267dd47b9668102960e883d9e7a74f674583c80
markusFrost <andreywystawkin@yandex.ru>
2016-07-12 20:52:42 +0300

    create model classes

---
+++ src/Book.java
@@ -0,0 +1,55 @@
+
+public class Book {
+
+	private String title;
+	
+	private String author;
+	
+	private int year;
+	
+	private int numberOfPages;
+	
+	private int overdueTime;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	public int getOverdueTime() {
+		return overdueTime;
+	}
+
+	public void setOverdueTime(int overdueTime) {
+		this.overdueTime = overdueTime;
+	}
+	
+	
+}
---
+++ src/Reader.java
@@ -0,0 +1,27 @@
+
+public class Reader {
+
+	private String name;
+	
+	private int readingSpeed;
+	
+	public Reader() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getReadingSpeed() {
+		return readingSpeed;
+	}
+
+	public void setReadingSpeed(int readingSpeed) {
+		this.readingSpeed = readingSpeed;
+	}
+	
+	
+}
